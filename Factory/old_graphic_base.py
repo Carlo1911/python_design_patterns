19a46cc8cbf9cf77278e67b6e9811ff11db956a0
Carlo1911 <carlo.alva@wunderman.com>
2020-09-27 23:01:44 -0500

    Factory

---
+++ Factory/old_graphic_base.py
@@ -0,0 +1,8 @@
+import pygame
+import time
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.draw.rect(screen, (255, 0, 34), pygame.Rect(42, 15, 40, 32))
+pygame.display.flip()
+time.sleep(10)
